7a6a9766c2cf05742bde0e01629ed96e892f9fcd
CalebNyachwaya <113683930+CalebNyachwaya@users.noreply.github.com>
2024-01-02 13:34:25 -0800

    add project files

---
+++ task_0/install_dependencies.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Initialize a new Node.js project
+npm init -y
+
+# Install Webpack and Webpack CLI as development dependencies
+npm install webpack webpack-cli --save-dev
+
+# Install jQuery as a regular dependency
+npm install jquery --save
+
